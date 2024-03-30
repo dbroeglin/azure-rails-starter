e38fad7432b9022abda29bdc0badb10059d89928
Dominique Broeglin <dominique.broeglin@gmail.com>
2024-03-30 22:31:42 +0100

    Renamed project to azure-rails-starter

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "azure_rails_demo_production"
+  # config.active_job.queue_name_prefix = "azure_rails_starter_production"
 
   config.action_mailer.perform_caching = false
 
